5b82fb4a84ab5125596b87332b25a1d2c1c397b8
silviaITI <silvia.casanovamartinez@gmail.com>
2023-12-17 12:50:59 +0100

    fviewModel

--- app/src/main/java/com/example/practica_fundamentosandroid_silviacasanova/MainActivity.kt
+++ app/src/main/java/com/example/practica_fundamentosandroid_silviacasanova/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.practica_fundamentosandroid_silviacasanova
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
+ val viewModel: MainActivityViewModel by viewModels()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Esto es un toast", Toast.LENGTH_LONG).show()
     }
 }
---
+++ app/src/main/java/com/example/practica_fundamentosandroid_silviacasanova/MainActivityViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.practica_fundamentosandroid_silviacasanova
+
+import androidx.lifecycle.ViewModel
+
+
+class MainActivityViewModel: ViewModel() {
+
+
+}
